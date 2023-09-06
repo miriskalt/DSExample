62d442d13e5e6c31f9b04fb04e1c52827b8e6d82
miriskalt <56968935+miriskalt@users.noreply.github.com>
2023-09-06 23:01:52 +0200

    Update app.py
    trying different code structure

--- app.py
+++ app.py
@@ -3,14 +3,12 @@ from dash_bootstrap_components.themes import BOOTSTRAP
 
 from src.components.layout import create_layout
 
-
-def main() -> None:
-    app = Dash(external_stylesheets=[BOOTSTRAP])
-    server = app.server
-    app.title = "Medal dashboard"
-    app.layout = create_layout(app)
-    app.run_server()
-
+app = Dash(external_stylesheets=[BOOTSTRAP])
+server = app.server
+app.title = "Medal dashboard"
+app.layout = create_layout(app)
+    
 
 if __name__ == "__main__":
-    main()
+    app.run_server(debug=False)
+
