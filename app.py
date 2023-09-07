105212bc4fa9502d5dd318f7362480ed9f392575
miriskalt <mirjam.bayer@gmx.de>
2023-09-07 08:30:33 +0200

    back to sample website

--- app.py
+++ app.py
@@ -1,21 +1,9 @@
 
-from dash import Dash, doc, dcc, html, Input, Output, callback
+from dash import Dash, dcc, html, Input, Output, callback
 import os
-import pandas as pd
-import plotly.express as px
 
 
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
-
-
-
-df = pd.read_csv('https://gist.githubusercontent.com/chriddyp/5d1ea79569ed194d432e56108a04d188/raw/a9f9e8076b837d541398e999dcbac2b2826a81f8/gdp-life-exp-2007.csv')
-
-fig = px.scatter(df, x="gdp per capita", y="life expectancy",
-                 size="population", color="continent", hover_name="country",
-                 log_x=True, size_max=60)
-
-
 
 
 app = Dash(__name__, external_stylesheets=external_stylesheets)
@@ -29,12 +17,7 @@ app.layout = html.Div([
         id='dropdown'
     ),
     html.Div(id='display-value'),
-    html.Div([
-    dcc.Graph(
-        id='life-exp-vs-gdp',
-        figure=fig
-    )
-    ])
+    html.H4('This city has ')
 ])
 
 @callback(Output('display-value', 'children'), Input('dropdown', 'value'))
