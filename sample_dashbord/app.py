3130c7626f1f2542fa68465cd4964297f4d0131d
miriskalt <mirjam.bayer@gmx.de>
2024-02-23 15:22:14 +0100

    sample_dashbord with multiple graphs

--- sample_dashbord/app.py
+++ sample_dashbord/app.py
@@ -12,14 +12,15 @@ server = app.server
 
 ### Import data
 
-df = pd.read_csv('./data/Takeout/Fitbit/')
-df = df[[' Matrikel_nr', ' Hauptfach', ' Fachsemester']]
+df = pd.read_csv('./data/sleep_stree_readiness.csv')
 
 
 ### App layout
 
 app.layout = html.Div([
     html.H1("This is a sample project using human data", style={'text-align': 'center'}),
+    html.H3("Linegraph of single feature over entire timespan", style={'text-align': 'center'}),
+    
     html.P('The following interactive linegraph visualizes the selected features over the entire timespan of the dataset'),
 
 
@@ -27,7 +28,7 @@ app.layout = html.Div([
                  options=[
                      {'label': "Deep Sleep in Minutes", 'value': 'deep_sleep_in_minutes'},
                      {'label': "Overall Score", 'value': 'overall_score'},
-                     {'label': "Resting Heart Rate", 'value': 'resting_heart_rate'}],
+                     {'label': "Readiness Score", 'value': 'readiness_score_value'}],
                  multi=False,
                  value='overall_score', # init value
                  style={'width': '40%'}
@@ -36,40 +37,81 @@ app.layout = html.Div([
     html.Div(id='output_container', children=[]),
     html.Br(), # whitespace
 
-    dcc.Graph(id='first_graph', figure={})
+    dcc.Graph(id='full_line_single_graph', figure={}),
 
-    ]
-)
+    html.H3("Linegraph of all features over entire timespan", style={'text-align': 'center'}),
+
+    dcc.Graph(id='full_line_all_graph', figure={}),
+
+
+    html.H3("Research Question 1: What is the avg Readiness Score per Weekday?", style={'text-align': 'center'}),
+    dcc.Graph(id='avg_readiness_day', figure={}),
+
+    ])
 
 ### Callbacks
 @app.callback(
     [Output(component_id='output_container', component_property='children'),
-     Output(component_id='first_graph', component_property='figure')],
+     Output(component_id='full_line_single_graph', component_property='figure')],
      [Input(component_id='feature_selection', component_property='value')]
 )
 def update_graph(option_selection):
-    print(option_selection)
-    print(type(option_selection))
 
     container = "The plot shows: {}".format(option_selection)
-
-    # update the data 
-    dff = df.copy()
-    print('copied')
-    dff = dff[option_selection]
-    # dff = dff[dff[' Hauptfach'] == option_selection]
-    # print('filtered', dff.shape)
     # create graph
-    fig = px.scatter(
-        data_frame = dff,
-        x = 'timestamp',
-        y = option_selection.get,
+    fig = px.line(
+        data_frame = df,
+        x = 'timeCode_x',
+        y = option_selection,
+        range_y=[0,100]        
     )
     return container, fig
 
 
 
-print("Ran through.")
+
+
+
+@app.callback(
+     Output(component_id='full_line_all_graph', component_property='figure'),
+     [Input(component_id='feature_selection', component_property='value')])
+def update_all_graph(toggled):
+    print('graph triggered')
+    
+    # create graph
+    fig = px.line(
+        data_frame = df,
+        x = 'timeCode_x',
+        y = ['STRESS_SCORE', 'overall_score', 'readiness_score_value'],
+        labels=['stress', 'sleep', 'readiness'],
+        range_y=[0,150]        
+    )
+    print('graph created')
+    return fig
+
+
+
+@app.callback(
+     Output(component_id='avg_readiness_day', component_property='figure'),
+     [Input(component_id='feature_selection', component_property='value')])
+def update_weekday_readiness_graph(toggled):
+    # TODO: ordering of the weekdays
+
+    fig = px.histogram(df,
+                       x="weekday", 
+                       y="readiness_score_value", 
+                       histfunc='avg', 
+                       range_y=[0,100],
+                       category_orders={"weekday":["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]}
+                       
+                       )
+    print('graph created')
+    return fig
+
+
+
+
+
 
 
 if __name__ == '__main__':
