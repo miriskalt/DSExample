f41335b0ee8d2600507b07417f070c3f8e5bdb81
miriskalt <mirjam.bayer@gmx.de>
2024-02-23 10:55:15 +0100

    api-tutorial added

--- app.py
+++ app.py
@@ -1,4 +1,3 @@
-
 import pandas as pd
 import dash
 from dash import dcc
@@ -13,29 +12,31 @@ server = app.server
 
 ### Import data
 
-df = pd.read_csv('data.csv')
+df = pd.read_csv('./data/Takeout/Fitbit/')
 df = df[[' Matrikel_nr', ' Hauptfach', ' Fachsemester']]
 
 
 ### App layout
 
 app.layout = html.Div([
-    html.H1("This is a heading", style={'text-align': 'center'}),
+    html.H1("This is a sample project using human data", style={'text-align': 'center'}),
+    html.P('The following interactive linegraph visualizes the selected features over the entire timespan of the dataset'),
 
-    dcc.Dropdown(id="select_something",
+
+    dcc.Dropdown(id="feature_selection",
                  options=[
-                     {'label': "First Option", 'value': 79},
-                     {'label': "Second Option", 'value': 277},
-                     {'label': "Third Option", 'value': 128}],
+                     {'label': "Deep Sleep in Minutes", 'value': 'deep_sleep_in_minutes'},
+                     {'label': "Overall Score", 'value': 'overall_score'},
+                     {'label': "Resting Heart Rate", 'value': 'resting_heart_rate'}],
                  multi=False,
-                 value=227, # init value
+                 value='overall_score', # init value
                  style={'width': '40%'}
                  ),
 
     html.Div(id='output_container', children=[]),
     html.Br(), # whitespace
 
-    dcc.Graph(id='a_graph', figure={})
+    dcc.Graph(id='first_graph', figure={})
 
     ]
 )
@@ -43,25 +44,26 @@ app.layout = html.Div([
 ### Callbacks
 @app.callback(
     [Output(component_id='output_container', component_property='children'),
-     Output(component_id='a_graph', component_property='figure')],
-     [Input(component_id='select_something', component_property='value')]
+     Output(component_id='first_graph', component_property='figure')],
+     [Input(component_id='feature_selection', component_property='value')]
 )
 def update_graph(option_selection):
     print(option_selection)
     print(type(option_selection))
 
-    container = "The option chosen is: {}".format(option_selection)
+    container = "The plot shows: {}".format(option_selection)
 
     # update the data 
     dff = df.copy()
     print('copied')
-    dff = dff[dff[' Hauptfach'] == option_selection]
-    print('filtered', dff.shape)
+    dff = dff[option_selection]
+    # dff = dff[dff[' Hauptfach'] == option_selection]
+    # print('filtered', dff.shape)
     # create graph
     fig = px.scatter(
         data_frame = dff,
-        x = ' Matrikel_nr',
-        y = ' Fachsemester',
+        x = 'timestamp',
+        y = option_selection.get,
     )
     return container, fig
 
